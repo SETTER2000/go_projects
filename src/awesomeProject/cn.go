4669dd9efdde055c7b8ba3c0c3cea61fc19f28cd
SETTER <info@lphp.ru>
2018-11-27 17:36:11 +0300

    Added all interface

--- src/awesomeProject/cn.go
+++ src/awesomeProject/cn.go
@@ -9,18 +9,19 @@ import (
 )
 
 var opts struct {
+	All                    bool `short:"a" long:"all" description:"Конвертировать во все доступные варианты."`
 	MeterToCentimeter      bool `short:"q" long:"mc" description:"Конвертировать метры в сантиметры."`
 	MeterToFoot            bool `short:"z" long:"mf" description:"Конвертировать метры в футы."`
-	MeterToMillimeter      bool `short:"х" long:"mmm" description:"Конвертировать метры в миллиметры."`
+	MeterToMillimeter      bool `short:"x" long:"mtomm" description:"Конвертировать метры в миллиметры."`
 	CentimeterToMeter      bool `short:"w" long:"cm" description:"Конвертировать сантиметры в метры."`
 	CentimeterToFoot       bool `short:"u" long:"cf" description:"Конвертировать сантиметры в футы."`
 	CentimeterToMillimeter bool `short:"v" long:"cmm" description:"Конвертировать сантиметры в миллиметры."`
 	FootToCentimeter       bool `short:"g" long:"fc" description:"Конвертировать футы в сантиметры."`
 	FootToMeter            bool `short:"j" long:"fm" description:"Конвертировать футы в метры."`
 	FootToMillimeter       bool `short:"b" long:"fmm" description:"Конвертировать футы в миллиметры."`
-	MillimeterToMeter      bool `short:"j" long:"fm" description:"Конвертировать миллиметры в метры."`
-	MillimeterToFoot       bool `short:"j" long:"fm" description:"Конвертировать миллиметры в футы."`
-	MillimeterToCentimeter bool `short:"j" long:"fm" description:"Конвертировать миллиметры в сантиметры."`
+	MillimeterToMeter      bool `short:"t" long:"mmtom" description:"Конвертировать миллиметры в метры."`
+	MillimeterToFoot       bool `short:"p" long:"mmf" description:"Конвертировать миллиметры в футы."`
+	MillimeterToCentimeter bool `short:"i" long:"mmc" description:"Конвертировать миллиметры в сантиметры."`
 }
 
 func main() {
@@ -36,6 +37,23 @@ func main() {
 		f := lengconv.Foot(d)
 		c := lengconv.Centimeter(d)
 		mm := lengconv.Millimeter(d)
+
+		if opts.All {
+			fmt.Printf(st+"ft\n"+st+"m\n"+st+"mm\n"+st+"cm\n"+st+"m\n"+st+"mm\n"+st+"ft\n"+st+"cm\n"+st+"mm\n"+st+"cm\n"+st+"ft\n"+st+"m\n",
+				c, lengconv.CmToFt(c),
+				c, lengconv.CmToM(c),
+				c, lengconv.CmToMm(c),
+				f, lengconv.FtToCm(f),
+				f, lengconv.FtToM(f),
+				f, lengconv.FtToMm(f),
+				m, lengconv.MToFt(m),
+				m, lengconv.MToCm(m),
+				m, lengconv.MToMm(m),
+				mm, lengconv.MmToCm(mm),
+				mm, lengconv.MmToFt(mm),
+				mm, lengconv.MmToM(mm),
+			)
+		}
 
 		if opts.MeterToCentimeter {
 			fmt.Printf(st+"см\n", m, lengconv.MToCm(m))
